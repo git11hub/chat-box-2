e75e9609f8c268f1691e184650cdec600f1061e2
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-05-29 23:17:19 +0600

    chat box 2

---
+++ src/features/Sidebar.js
@@ -0,0 +1,33 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import React from 'react';
+import "./Sidebar.css";
+import SearchIcon from "@material-ui/icons/Search";
+import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined";
+import SidebarChat from './SidebarChat';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar_header">
+                <Avatar className="sidebar_avatar" />
+                <div className="sidebar_input">
+                    <SearchIcon />
+                    <input placeholder="Search" />
+                </div>
+                
+                <IconButton variant="outlined" className="sidebar_inputButton">
+                 <RateReviewOutlinedIcon />
+                </IconButton>
+                
+            </div>
+  
+            <div className="sidebar_chats">
+                <SidebarChat /> 
+                <SidebarChat /> 
+                <SidebarChat />                 
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/features/SidebarChat.js
@@ -0,0 +1,18 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import "./SidebarChat.css";
+
+function SidebarChat() {
+    return (
+        <div className="sidebarChat">
+            <Avatar />
+            <div className="sidebarChat_info">
+                <h3>Channel Name</h3>
+                <p>Last message sent...</p>
+                <small>timestamp</small>
+            </div>
+        </div>
+    )
+}
+
+export default SidebarChat;
