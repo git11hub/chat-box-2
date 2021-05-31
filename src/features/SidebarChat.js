94d227d210d3ed882ac647c10005db327bec80ab
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-05-31 13:55:11 +0600

    channel avatar image updated

--- src/features/SidebarChat.js
+++ src/features/SidebarChat.js
@@ -1,12 +1,23 @@
 import { Avatar } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setChat } from './chatSlice';
+import db from './firebase';
 import "./SidebarChat.css";
 
 function SidebarChat({ id, chatName }) {
     const dispatch = useDispatch();
-    const [chatInfo, setChatInfo] = useState([]);
+    const [chatInfo, setChatInfo] = useState([]); 
+
+    useEffect(() => {
+        db.collection("chats")
+        .doc(id)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) =>
+            setChatInfo(snapshot.docs.map((doc) => doc.data()))
+        );
+    }, [id])
 
     return (
         <div onClick={() =>
@@ -17,11 +28,11 @@ function SidebarChat({ id, chatName }) {
                 })
             )
         } className="sidebarChat">
-            <Avatar />
+            <Avatar src={chatInfo[0]?.photo} />
             <div className="sidebarChat_info">
                 <h3>{chatName}</h3>
-                <p>Last message sent...</p>
-                <small>timestamp</small>
+                <p>{chatInfo[0]?.message}</p>
+                <small>{new Date(chatInfo[0]?.timestamp?.toDate()).toLocaleString()}</small>
             </div>
         </div>
     )
